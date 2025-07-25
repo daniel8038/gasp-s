27b22c06a93c7506023b1abe84a3aef050c3373f
zzh <zzh@zzhdeMacBook-Pro.local>
2025-07-25 21:20:30 +0800

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+
+      <HeroSection />
+    </main>
+  );
+}
+
+export default App;
